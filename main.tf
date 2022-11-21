3a6e881862bfbf8b18bcee5d32ea234a228260fb
silviurm <103998741+silviurm@users.noreply.github.com>
2022-11-21 17:50:36 +0200

    changed to 2d

--- main.tf
+++ main.tf
@@ -21,7 +21,7 @@ module "vpc" {
   cidr = "10.0.0.0/16"
 
   azs             = ["us-west-2a", "us-west-2b", "us-west-2d"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.104.0/24"]
 
   enable_nat_gateway = true
 
@@ -35,7 +35,7 @@ resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
 
-  
+
   vpc_security_group_ids = [module.blog_sg.security_group_id]
 
   tags = {
