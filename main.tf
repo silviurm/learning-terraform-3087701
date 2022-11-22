8ff6462e5a732fa9a5242bac3579af119a18ff4e
silviurm <103998741+silviurm@users.noreply.github.com>
2022-11-22 09:42:42 +0200

    scratch2

--- main.tf
+++ main.tf
@@ -20,7 +20,7 @@ module "vpc" {
   name = "dev"
   cidr = "10.0.0.0/16"
 
-  azs             = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
+  azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
   enable_nat_gateway = true
@@ -48,7 +48,7 @@ module "blog_sg" {
   version = "4.16.2"
 
   name = "blog"
-  vpc_id = module.vpc.public_subnets[2]
+  vpc_id = module.vpc.public_subnets[0]
 
   ingress_rules = ["http-80-tcp","https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
